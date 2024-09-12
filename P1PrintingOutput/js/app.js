d10a2d2e4946628f3dd8eab3ac47103d6fc96252
emmkro <emmkro167@ihmeducation.se>
2024-09-12 11:08:43 +0200

    P1 Printing Output

--- P1PrintingOutput/js/app.js
+++ P1PrintingOutput/js/app.js
@@ -1,3 +1,17 @@
-console.log("Hello World");
-console.log("My name is Emma");
+// This a comment.
 
+console.log("First");
+console.log("My name is Emma");
+console.log("hey")
+
+const five =5;
+const eight =8;
+let total=0;
+
+
+console.log (five);
+console.log(eight);
+//5+8= 13
+total = five + eight;
+console.log(total);
+console.log(five+eight);
