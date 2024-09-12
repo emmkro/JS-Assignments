b22afbf361e714153bf44c2ff82d7a1ffa29241d
emmkro <emmkro167@ihmeducation.se>
2024-09-12 14:42:39 +0200

    app2.js

---
+++ P5Operators/js/vendor/app2 .js
@@ -0,0 +1,71 @@
+// Using 3 variables and storing them in answer
+
+let x= 5+ 5;//x = 10
+let y= x+5;//y = 10
+let z= y+y; //z = 30
+console.log(z+z)
+
+let firstNumber= 7;
+let secondNumber = 2;
+let seven = 7;
+let answer = 0
+//1+2=3
+answer = firstNumber+secondNumber;
+
+console.log(answer);
+
+answer = firstNumber- secondNumber;
+console.log (answer);
+
+answer =firstNumber * secondNumber;
+console.log (answer);
+
+answer= firstNumber / secondNumber;
+console.log(answer);
+
+answer= firstNumber % secondNumber;
+console.log(answer);
+
+//MODIFYING VARIABLES WITH NUMBERS
+let gold = 50;
+gold + 10;
+console.log(gold); //50
+
+gold = gold + 10; // Long version of it
+console.log(gold); // 60
+
+gold += 10; // how to short write it
+console.log(gold); // 70
+
+gold *= 10;
+console.log(gold); // 700
+
+gold/= 10;
+console.log(gold);
+
+gold-=10;
+console.log(gold);
+
+gold %= 2;
+console.log(gold); //0
+
+//PRE OR POST PREDICLEMENTS, PLUS OCH MINUS
+let numberOfStudents = 0;
+
+numberOfStudents+= 1;
+console.log(numberOfStudents); // 2
+
+numberOfStudents++;
+console.log(numberOfStudents); //1
+
+//numberOfStudents--;
+//console.log(numberOfStudents); //1
+
+++numberOfStudents;
+console.log(numberOfStudents); //3
+
+console.log(numberOfStudents++) //3
+console.log(numberOfStudents); //4
+console.log(++numberOfStudents); // 5 :Position of ++ or -- is critical of which result you will get
+
+
