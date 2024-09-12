ba9071b5b578d35abb061acaaa400a655789a17f
emmkro <emmkro167@ihmeducation.se>
2024-09-12 15:17:27 +0200

    app2.js

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -1,1 +1,26 @@
+let speedInput= 72;
+let speedOuput= 0;
+speedOuput= speedInput /3.6;
+console.log (speedOuput);
+
+let minutesInput=3 ;
+let secondsOutput= 0;
+
+secondsOutput=minutesInput *60;
+console.log (secondsOutput);
+
+let numberInput = 11;
+let numberOutput= 4;
+answer= numberInput / numberOutput;
+console.log(answer);
+
+answer= numberInput % numberOutput;
+console.log(answer)
+
+let radiusInput = 2;
+let piOutput= 3.14;
+area= radiusInput * radiusInput * piOutput;
+console.log(area);
+
+
 
