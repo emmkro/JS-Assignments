a5288afdf20d9d70400f9bdd0e1b1f54dd90c584
emmkro <emmkro167@ihmeducation.se>
2024-09-16 11:52:34 +0200

    update P7 boolean

--- P7Boolean/js/app.js
+++ P7Boolean/js/app.js
@@ -7,8 +7,23 @@ let isChild= (ageOfPerson >= 0) && (ageOfPerson <=10);
 let isTeenager= (ageOfPerson >= 11) && (ageOfPerson<= 17);
 let isAdult= ageOfPerson >= 18;
 
-
 console.log (isChild);
 console.log(isTeenager);
 console.log(isAdult);
 
+let playerTouchGround = false;
+let playerHasJetPack= true;
+
+if (playerTouchGround){
+  console.log("player is on the ground.");
+console.log("Player can jump");
+}
+
+else if (playerHasJetPack) {
+  console.log("player is not touching the ground.");
+  console.log("you cannot jump right now") }
+
+else{
+  console.log("Playser is NOT on the ground!");
+  console.log("player CANNOT jup!");
+}
