4b4605af61336c91bbb56d269644edebab6267b4
emmkro <emmkro167@ihmeducation.se>
2024-09-16 13:36:13 +0200

    update P7 boolean

--- P7Boolean/js/app.js
+++ P7Boolean/js/app.js
@@ -1,5 +1,6 @@
-const birthyear= 2010;
+const birthyear= 2003;
 const currentYear= 2024;
+const favoriteNumber= 21;
 let ageOfPerson= currentYear - birthyear;
 console.log (ageOfPerson);
 
@@ -7,9 +8,37 @@ let isChild= (ageOfPerson >= 0) && (ageOfPerson <=10);
 let isTeenager= (ageOfPerson >= 11) && (ageOfPerson<= 17);
 let isAdult= ageOfPerson >= 18;
 
+
+if(isChild) {
+  console.log ("you are a child");
+}
+if(isTeenager) {
+ console.log ("you are a teenager");
+}
+else if (isAdult) {
+  console.log ("you are a adult");
+}
+console.log(favoriteNumber);
+
+if (favoriteNumber > ageOfPerson) {
+  console.log("favorite number is greater");
+}
+else if (favoriteNumber == ageOfPerson) {
+  console.log("they are the same")
+}
+   else {
+     console.log("age is greater")
+}
+
+
+
 console.log (isChild);
 console.log(isTeenager);
 console.log(isAdult);
+
+
+
+
 
 let playerTouchGround = false;
 let playerHasJetPack= true;
@@ -27,5 +56,6 @@ else{
   console.log("Player is NOT on the ground!");
   console.log("player CANNOT jup!");
 }
+
 
 
