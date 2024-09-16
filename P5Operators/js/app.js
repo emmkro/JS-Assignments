afb007d44f7301998997d433cad50126b93c8727
emmkro <emmkro167@ihmeducation.se>
2024-09-16 10:01:45 +0200

    App.Js

--- P5Operators/js/app.js
+++ P5Operators/js/app.js
@@ -1,26 +1,89 @@
+/*
+//1
 let speedInput= 72;
-let speedOuput= 0;
-speedOuput= speedInput /3.6;
-console.log (speedOuput);
+let speedOutput= 0;
+speedOutput= speedInput /3.6;
+console.log (speedOutput);
 
+//2
 let minutesInput=3 ;
 let secondsOutput= 0;
 
 secondsOutput=minutesInput *60;
 console.log (secondsOutput);
 
-let numberInput = 11;
-let numberOutput= 4;
-answer= numberInput / numberOutput;
+//3
+let devisionInput1 = 11;
+let divisionOutput2= 4;
+answer= devisionInput1 / divisionOutput2;
 console.log(answer);
 
-answer= numberInput % numberOutput;
+answer= devisionInput1 % divisionOutput2;
 console.log(answer)
 
+//4
 let radiusInput = 2;
 let piOutput= 3.14;
 area= radiusInput * radiusInput * piOutput;
 console.log(area);
+
+//5
+let input1= 11
+let output2= -1
+answer= input1 * (output2);
+console.log(answer);
+
+//6
+let weightInput= 70
+let heightOutput= 1.82
+BMI= weightInput/ (heightOutput * heightOutput);
+console.log (BMI)
+//or
+
+let weightInKg= 70
+let heightInMeters= 1.82
+bmi= weightInKg / Math.pow (heightInMeters, 2)
+console.log(bmi)
+*/
+
+//5
+let secondInput= 111;
+let minSecOutput= 60
+
+let minOutput;
+minOutput= Math.floor(secondInput / 60);
+console.log(minOutput);
+
+let secOutput3;
+secOutput3= secondInput - minOutput * 60;
+console.log(secOutput3);
+minsecOutput= minOutput + secOutput3;
+
+let secOutput3;
+secOutput1= secondInput- minOutput * 60;
+console.log(secOutput3);
+minsecOutput = minOutput + "minute(s) and" + secOutput3 + "second(s)";
+console.log(minsecOutput);
+
+
+/*
+let sideA= 3;
+let sideB= 4;
+let hypotenuse;
+let squareofAandB= Math.pow(sideA, 2) + Math.pow(sideB, 2);
+hypotenuse= Math.sqrt(squareofAandB);
+console.log(hypotenuse);
+*/
+
+
+
+
+
+
+
+
+
+
 
 
 
