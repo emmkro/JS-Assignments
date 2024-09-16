0182318c2e7e31ee6c42bad3b2596ea6a91b6c80
emmkro <emmkro167@ihmeducation.se>
2024-09-16 10:25:04 +0200

    app3.Js

---
+++ P5Operators/js/app3.js
@@ -0,0 +1,14 @@
+
+let numberofBottles= 10;
+const line1= " green bottles hangning on the wall, ";
+const line2= " and if one of the greens bottle accidently fall there will be "
+
+console.log(numberofBottles + line1 + numberofBottles + line1);
+console.log(line2 + --numberofBottles + line1);
+
+
+
+
+
+
+
