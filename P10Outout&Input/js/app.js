c799f5efb2a87c8fe03ccbafb003208aa3f9547a
emmkro <emmkro167@ihmeducation.se>
2024-09-17 11:39:50 +0200

    update uppgift

--- P10Outout&Input/js/app.js
+++ P10Outout&Input/js/app.js
@@ -17,15 +17,54 @@ let Number2= 6;
 function numberToDuplicate(number){
   return (Number1 + Number2) /2;
 }
+//OR
 console.log (numberToDuplicate (Number1+ Number2));
+
+function avarage(firstNumber, secondNumber){
+  return (firstNumber + secondNumber) /2;
+}
+console.log(avarage(2, 6));
 
 //
 
 function welcomeResult(firstname, lastname){
-  return"Welcome " +" "+ firstname + " " + lastname;
+  return"Welcome " + firstname + " " + lastname;
 }
-console.log(welcomeResult("Emma", "Kronvist"));
+console.log(welcomeResult("Emma", "Kronkvist"));
+
 
 //
 
-function randomNumber(0, 2)
+function randomNumber() {
+  return Math.floor(Math.random() * Math.floor(3));
+}
+console.log(randomNumber());
+
+function convertNumbersToLetters(number){
+  if(number === 0){
+    return "A";
+  }
+  if(number === 1){
+    return "B";
+  }
+if (number === 2){
+  return "C";
+}
+}
+console.log(convertNumbersToLetters(0));
+
+//
+function evulateResult(firstNumber, secondNumber){
+  let string;
+  if (firstNumber === secondNumber){
+     string= "Both numbers are equal!";
+  }
+ if (firstNumber > secondNumber){
+    string= "First number i greater!";
+ }
+ if (firstNumber < secondNumber){
+   string= "First number is smaller!";
+ }
+ return string;
+}
+console.log(evulateResult (5 , 2));
