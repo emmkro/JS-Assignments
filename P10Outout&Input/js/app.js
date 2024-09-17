97c4b195dec696a247f83297dd4dadad8acdf834
emmkro <emmkro167@ihmeducation.se>
2024-09-17 10:41:24 +0200

    input and output exercisie

---
+++ P10Outout&Input/js/app.js
@@ -0,0 +1,31 @@
+let inputNumber= 4;
+function multiplyBySelf(numberToMultiply){
+
+  return numberToMultiply * numberToMultiply;
+}
+console.log(multiplyBySelf(4));
+
+function add(firstNumber, secondNumber){
+  return firstNumber + secondNumber;
+}
+console.log(add(5, 20));
+
+//
+
+let Number1= 2;
+let Number2= 6;
+function numberToDuplicate(number){
+  return (Number1 + Number2) /2;
+}
+console.log (numberToDuplicate (Number1+ Number2));
+
+//
+
+function welcomeResult(firstname, lastname){
+  return"Welcome " +" "+ firstname + " " + lastname;
+}
+console.log(welcomeResult("Emma", "Kronvist"));
+
+//
+
+function randomNumber(0, 2)
