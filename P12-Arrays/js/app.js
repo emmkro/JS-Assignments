b803b0c37e1901e9ac0a73101482698216e5cdbd
emmkro <emmkro167@ihmeducation.se>
2024-09-23 13:33:20 +0200

    a12

--- P12-Arrays/js/app.js
+++ P12-Arrays/js/app.js
@@ -1,2 +1,13 @@
 
+let randomNumberCount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+randomNumberCount[0] += 1;
+
+for (let i = 0; i < 100; i++) {
+  console.log (getRandomInt (10));
+}
+
+function getRandomInt( max) {
+  return Math.floor(Math.random() * max );
+}
 
